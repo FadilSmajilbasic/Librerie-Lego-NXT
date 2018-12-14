54126f1786cd1c2ae5a222c2b1403b29e1d00688
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-12-14 16:20:43 +0100

    Update Wait lib

--- Codice/simpleTouchLib.h
+++ Codice/simpleTouchLib.h
@@ -1,11 +1,3 @@
 bool isPressed(short port){
-	bool preState = 0;
-
-	while(true){
-		bool currentState = SensorValue(port);
-
-		if(preState == 0 && currentState == 1){
-			return true;
-		}
-	}
+	return (bool) SensorValue(port);
 }
