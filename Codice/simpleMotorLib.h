3a234d22114929473e34d4b3d54841734a35dc92
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-01-25 14:13:12 +0100

    Minor fixes

--- Codice/simpleMotorLib.h
+++ Codice/simpleMotorLib.h
@@ -12,40 +12,30 @@
 /// </remarks>
 #endif
 
-//Funziona
-void goMotorSeconds(short port, float seconds , byte power = 127)
-{
-	motor[port] = power;
-	wait1Msec(1000);
-}
-
-//Funziona
 void goMotorStandard(short port, byte power = 127)
 {
 	motor[port] = power;
 }
 
-//Deve essere testato
+void goMotorSeconds(short port, float seconds , byte power = 127)
+{
+	goMotorStandard(port,power)
+	wait1Msec(1000);
+}
+
 void goMotorDegrees(short motorId, float degrees, byte power = 127)
 {
 	setMotorTarget(motorId, degrees, power);
 	waitUntilMotorStop(motorId);
 }
 
-//Deve essere testato
 void goMotorRotations(short motorId, int giri, byte power = 127){
 	for(int i = 0; i < giri; i++){
 		goMotorDegrees(motorId,360,127);
 	}
 }
 
-//Funziona -> se mode = 1: instant, se 0: friction (spegne i motori e si ferma da solo
-void stopMotor(short motorId, short mode = 1)
+void stopMotor(short motorId)
 {
-	if(mode == 1){
-		motor[motorId] = 0;
-	}
-	else{
-
-	}
+	motor[motorId] = 0;
 }
